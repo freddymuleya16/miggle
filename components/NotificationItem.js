74fb41616774a258b27e9b2e4a1c9866ec4d6dca
Freddy Muleya <80630982+freddymuleya16@users.noreply.github.com>
2023-05-02 18:52:50 +0200

    Notification_2023_05_02

--- components/NotificationItem.js
+++ components/NotificationItem.js
@@ -1,32 +1,76 @@
-import Image from "next/image";
-import React from "react";
-import { Badge, Button, Media } from "react-bootstrap";
+import { markAsRead } from "@/actions/notificationActions";
+import { db } from "@/utils/firebase";
+import { getAuth } from "firebase/auth";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { Badge, Dropdown, Nav, NavDropdown } from "react-bootstrap";
+import { FaBell } from "react-icons/fa";
+import { useDispatch } from "react-redux";
 
-const NotificationItem = ({ message, time, read }) => {
+export default function NotificationItem() {
+  const [show, setShow] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+  const dispatch = useDispatch();
+  // Listen for changes in messages when a chat is opened
+  useEffect(() => {
+    // Use onSnapshot to listen for changes in the messages collection for this chat
+    const unsubscribe = onSnapshot(
+      query(
+        collection(
+          db,
+          `notification/${getAuth().currentUser.uid}/notifications`
+        ),
+        orderBy("timestamp", "asc")
+      ),
+      (querySnapshot) => {
+        const notifications = [];
+        // Loop through each document in the messages collection and add it to the messages array
+        querySnapshot.forEach((doc) => {
+          notifications.push({ ...doc.data(), id: doc.id });
+        });
+        setNotifications([...notifications]);
+      }
+    );
+    return () => unsubscribe();
+  }, []);
+
+  const handleRead = (id) => {
+    {
+      dispatch(markAsRead(id));
+    }
+  };
   return (
-    <div className={`py-3 media ${read ? "" : "bg-light"}`}>
-      
-      {/* <Image
-      src="https://via.placeholder.com/64"
-      alt="notification sender"
-      width={400}
-      height={400}
-      className="mr-3 rounded-circle"
-      /> */}
-      <div className="media-body">
-        <h5>{message}</h5>
-        <p className="text-muted mb-0">{time}</p>
-      </div>
-      {!read && (
-        <div className="ml-3">
-          <Badge variant="primary">New</Badge>
-        </div>
+    <NavDropdown
+      title={
+        <>
+          <FaBell className="me-1" />
+          {notifications.filter((n) => !n.read).length > 0 && (
+            <Badge bg="danger">
+              {notifications.filter((n) => !n.read).length}
+            </Badge>
+          )}
+        </>
+      }
+      ali
+      align="end"
+      menuAlign ='end'
+      id="basic-nav-dropdown"
+      className="py-1"
+    >
+      {notifications.length === 0 && (
+        <NavDropdown.Item disabled>No notifications yet</NavDropdown.Item>
       )}
-      <Button variant="link" className="ml-3">
-        View
-      </Button>
-    </div>
+      {notifications.map((notification) => (
+        <>
+          <Dropdown.Item
+            key={notification.id}
+            onClick={() => handleRead(notification.id)}
+            className={`fw-bold ${notification.read ? "text-muted" : ""}`}
+          >
+            {notification.message}
+          </Dropdown.Item>
+        </>
+      ))}
+    </NavDropdown>
   );
-};
-
-export default NotificationItem;
+}
