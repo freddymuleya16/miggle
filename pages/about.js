d8206532c55fc4bf0c8c7bc548d491421f045e02
Freddy Muleya <freddymuleya16@gmail.com>
2023-07-11 11:46:23 +0200

    1s

--- pages/about.js
+++ pages/about.js
@@ -1,53 +1,57 @@
-import MainLayout from "@/components/MainLayout";
+import WelcomeLayout from "@/components/WelcomeLayout";
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
 
-const About = () => {
+const AboutUs = () => {
   return (
-    <>
-      <Container>
-        <Row className="my-4">
-          <Col>
-            <h1>About Us</h1>
-            <p>
-              We are Mingle, a dating site that connects people from all over
-              the world. Our goal is to create a safe and enjoyable space where
-              people can find their perfect match.
+    <WelcomeLayout>
+      
+
+        <div className="container mx-auto py-8">
+          <div className="my-4">
+            <h1 className="text-3xl font-bold text-white">About Us</h1>
+            <p className="text-lg text-white">
+              Welcome to Mingle, the ultimate online dating platform. We are
+              dedicated to helping individuals find love, meaningful
+              connections, and lifelong companionship. With Mingle, you can
+              explore a world of possibilities and meet like-minded individuals
+              who share your interests and values.
             </p>
-          </Col>
-        </Row>
-        <Row className="my-4">
-          <Col md={6}>
-            <h2>Our Mission</h2>
-            <p>
-              Our mission is to create a dating site that is inclusive, fun, and
-              easy to use. We want to help people find love, companionship, and
-              meaningful connections.
+          </div>
+          <div className="my-4">
+            <h2 className="text-2xl font-bold text-white">Our Mission</h2>
+            <p className="text-lg text-white">
+              At Mingle, our mission is to create a safe, inclusive, and
+              enjoyable space where individuals can connect with others,
+              fostering meaningful relationships. We strive to provide a
+              user-friendly platform that enables our users to explore and
+              discover potential matches while prioritizing their privacy and
+              security.
             </p>
-          </Col>
-          <Col md={6}>
-            <h2>Our Vision</h2>
-            <p>
-              Our vision is to become the most trusted and respected dating site
-              in the world. We want to be known for our innovative features,
-              exceptional customer service, and commitment to our users.
+          </div>
+          <div className="my-4">
+            <h2 className="text-2xl font-bold text-white">Our Vision</h2>
+            <p className="text-lg text-white">
+              Our vision is to become the leading online dating platform,
+              recognized for our exceptional user experience, advanced matching
+              algorithms, and commitment to user satisfaction. We aim to create
+              an environment where individuals can find genuine connections and
+              build meaningful relationships that can stand the test of time.
             </p>
-          </Col>
-        </Row>
-        <Row className="my-4">
-          <Col>
-            <h2>Our Team</h2>
-            <p>
-              We are a team of experienced developers, designers, and dating
-              experts who are passionate about helping people find love. Our
-              team is dedicated to providing the best possible experience for
-              our users.
-            </p>
-          </Col>
-        </Row>
-      </Container>
-    </>
+          </div>
+          <div className="my-4">
+            <h2 className="text-2xl font-bold text-white">Why Choose Us</h2>
+            <ul className="list-disc list-inside text-lg text-white">
+              <li>Extensive user base with diverse profiles</li>
+              <li>Advanced matching algorithms for accurate compatibility</li>
+              <li>User-friendly interface for easy navigation</li>
+              <li>Secure and private platform to protect your information</li>
+              <li>24/7 customer support for assistance and guidance</li>
+            </ul>
+          </div>
+        </div>
+        
+    </WelcomeLayout>
   );
 };
 
-export default About;
+export default AboutUs;
