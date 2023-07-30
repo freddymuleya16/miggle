fb32b7d6a68df0be932da49a35d0432c6cb26309
Freddy Muleya <freddymuleya16@gmail.com>
2023-07-30 17:28:33 +0200

    Pics/Vids

--- components/Message/Messages.js
+++ components/Message/Messages.js
@@ -24,11 +24,13 @@ import { getUser } from '@/actions/authActions';
 import FullscreenLoading from '../FullscreenLoading';
 import OverLayLoading from '../OverLayLoading';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowAltCircleLeft, faHome, faHomeAlt, faLocationDot, faMars, faVenus, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleLeft, faHome, faHomeAlt, faImages, faLocationDot, faMars, faTrash, faTrashAlt, faVenus, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FaHome } from 'react-icons/fa';
 import ChatList from './ChatList';
 import UserMessageAction from '../UserMessageAction';
 import { decryptMessage, encryptMessage, getChatDocument } from '@/utils/helpers';
+import FileUploader from './FileUploader';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 
 
 
@@ -140,15 +142,36 @@ function Messages({ receiverId, matchDate, updateCurrentMatch, user }) {
     // Handle the form submission to send a new message
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const newMessage = {
-            sender: getAuth().currentUser.uid,
-            message: encryptMessage(message),
-            timestamp: serverTimestamp(),
-            liked: false
-        };
-        // setMessages([...messages, newMessage]);
-        setMessage("");
-        await addMessage(chatRef, newMessage);
+        setLoading(true)
+        try {
+            if (message == '' && selectedFiles.length == 0) return;
+
+            const filteredVideos = selectedFiles.filter(x => x.type == "video/mp4")
+            const filteredImages = selectedFiles.filter(x => x.type !== "video/mp4")
+            const [videos, images] = await Promise.all(
+                [
+                    handleUpload(filteredVideos),
+                    handleUpload(filteredImages),
+                ])
+             
+            const newMessage = {
+                sender: getAuth().currentUser.uid,
+                message: encryptMessage(message),
+                timestamp: serverTimestamp(),
+                liked: false,
+                videos,
+                images
+            };
+            console.log(videos, images)
+            // setMessages([...messages, newMessage]);
+            setMessage("");
+            await addMessage(chatRef, newMessage);
+        }
+        catch (error) {
+            console.error(error)
+        } finally {
+            setLoading(false)
+        }
     };
 
     const removeMatch = async (userId, matchId) => {
@@ -243,6 +266,79 @@ function Messages({ receiverId, matchDate, updateCurrentMatch, user }) {
             setLoading(false)
         }
     };
+    const [selectedFiles, setSelectedFiles] = useState([]);
+    const [uploadProgress, setUploadProgress] = useState(0);
+    const [currentFile, setCurrentFile] = useState(0)
+
+    const handleFileChange = (e) => {
+        const newObjectUrls = [...e.target.files].map((pic) => {
+            pic.preview = window.URL.createObjectURL(pic)
+            return pic
+        });
+
+        setSelectedFiles([...newObjectUrls])
+    };
+
+    const handleUpload = async (files) => {
+        if (!files) return [];
+        if (files.length == 0) return [];
+        const storage = getStorage();
+
+        const pictureRefs = await Promise.all(
+            files.map((picture) => {
+
+                const fileRef = ref(
+                    storage,
+                    `Chats/${Date.now()}-${picture.name}`
+                );
+                const uploadTask = uploadBytesResumable(fileRef, picture)
+
+
+                uploadTask.on('state_changed',
+                    (snapshot) => {
+                        // Observe state change events such as progress, pause, and resume
+                        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        setUploadProgress(progress);
+                        switch (snapshot.state) {
+                            case 'paused':
+                                //console.log('Upload is paused');
+                                break;
+                            case 'running':
+                                //console.log('Upload is running');
+                                break;
+                        }
+                    },
+                    (error) => {
+                        // Handle unsuccessful uploads
+                        console.error('Error uploading file:', error);
+
+                    },
+                    async () => {
+                        try {
+                            //const pictureRef = await getDownloadURL(fileRef);
+                            //console.log('pics', pictureRef)
+                        } catch (error) {
+                            console.error("Error adding message:", error);
+                        }
+                        setSelectedFiles([]);
+                        setUploadProgress(0);
+
+                        // Handle successful uploads on complete
+                        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                        // return await getDownloadURL(uploadTask.snapshot.ref)
+                    }
+                );
+
+                return uploadTask.then((snapshot) =>
+                    getDownloadURL(snapshot.ref)
+                );
+            }))
+
+        return pictureRefs;
+    };
+
 
     if (receiver == null || currentUser == null) {
         return <OverLayLoading />
@@ -271,24 +367,81 @@ function Messages({ receiverId, matchDate, updateCurrentMatch, user }) {
 
                     </div>
                     <div className="overflow-auto h-80-p scroll-botton">
-                        <ChatList groupedChats={groupedMessages} receiver={receiver} currentUser={user} chatRef={chatRef} />
-                        {/* {messages.map((message, index) =>
-                            <Chat key={index} data={{ pictures: receiver.pictures, ...message, isCurrent: message.sender == currentUser.id, gender: message.sender == currentUser.id ? currentUser.gender : receiver.gender, chatRef }} />
+                        {!(selectedFiles.length > 0) && <ChatList groupedChats={groupedMessages} receiver={receiver} currentUser={user} chatRef={chatRef} />}
+                        {
+                            selectedFiles.length > 0 &&
+                            (
+                                selectedFiles[currentFile].type == "video/mp4" ?
+                                    <div className="  h-full items-center object-Cover w-full" >
+                                        <video autoPlay controls loop className=" w-full  object-Cover" >
+                                            <source src={selectedFiles[currentFile].preview} />
+                                        </video>
+                                    </div>
 
-                        )} */}
+                                    :
+                                    <Image width={500} height={500} src={selectedFiles[currentFile]?.preview ?? window.URL.createObjectURL(selectedFiles[currentFile])} alt="Selected Image" className=" w-full h-full object-Cover" />
+                            )
+                        }
+                        {
+                            selectedFiles.length > 0 && <div className='flex justify-center -mt-28 mb-3   overflow-y-auto mx-2'>
+                                {selectedFiles.map((pic, i) => (
+                                    <div key={i} className={`relative justify-center w-20 h-20 rounded-md  mr-2  ${i == currentFile ? 'border-4 border-rose-500' : 'border-2'}`}>
+                                        <FontAwesomeIcon
+                                            onClick={() => {
+                                                const updatedPictures = [...selectedFiles];
+                                                updatedPictures.splice(i, 1);
+                                                console.log(currentFile, updatedPictures.length)
+                                                if (currentFile > updatedPictures.length - 1) {
+                                                    setCurrentFile(updatedPictures.length - 1)
+                                                }
+                                                setSelectedFiles(updatedPictures);
+                                            }}
+                                            size="md"
+                                            icon={faTrashAlt}
+                                            className="absolute top-1 right-1 text-[#fff] cursor-pointer   rounded-full p-1 hover:text-red-500"
+                                        />
+                                        {pic.type == "video/mp4" ?
+                                            <video onClick={() => setCurrentFile(i)} muted autoPlay={false} className={`cursor-pointer rounded-sm  w-full h-full object-cover`} >
+                                                <source src={pic.preview} />
+                                            </video>
+                                            :
+                                            <Image width={500} onClick={() => setCurrentFile(i)} height={500} src={pic.preview} alt="Selected Image" className={`cursor-pointer rounded-sm  w-full h-full object-cover`} />
+                                        }
+                                    </div>
+                                ))}
+                            </div>
+
+                        }
                     </div>
 
 
                 </div>
+
+
                 <div className=" h-[10vh] px-4 border-t-2 border-gray-400 bg-gray-200 fixed bottom-0 left-0 right-0 sm:relative">
-                    <input value={message}
-                        onChange={(event) => setMessage(event.target.value)} placeholder="Type a message ..." className="h-full w-5/6 bg-transparent  font-poppins  border-none outline-none placeholder-gray-500 text-lg" />
-                    <button
-                        onClick={handleSubmit}
-                        className="w-1/6 h-4/6 rounded-full font-poppins bg-gradient-to-r from-rose-500 to-rose-300 text-white py-2 px-2 sm:px-4 hover:opacity-75 focus:outline-none"
-                    >
-                        Send
-                    </button>
+
+                    <div className=' w-full flex h-full items-center'>
+                        <label className="flex flex-col items-center justify-center  cursor-pointer">
+                            <FontAwesomeIcon icon={faImages} size='xl' className='mr-2 text-gray-600 cursor-pointer ' />
+
+                            <input
+                                type="file"
+                                className="hidden"
+                                accept="image/*, video/*"
+                                multiple={true}
+                                onChange={handleFileChange}
+                            />
+                        </label>
+                        <input value={message}
+                            onChange={(event) => setMessage(event.target.value)} placeholder="Type a message ..." className="flex-grow  bg-transparent  font-poppins  border-none outline-none placeholder-gray-500 text-lg" />
+                        <button
+                            onClick={handleSubmit}
+                            className="w-1/6 h-4/6 rounded-full font-poppins bg-gradient-to-r from-rose-500 to-rose-300 text-white py-2 px-2 sm:px-4 hover:opacity-75 focus:outline-none"
+                        >
+                            Send
+                        </button>
+                    </div>
+
                 </div>
 
             </div>
